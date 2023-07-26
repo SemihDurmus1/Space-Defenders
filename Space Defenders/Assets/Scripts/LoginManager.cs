4cbbdfa09fb8a6e5b0d61b4f33874bd441ccafbe
SemihDurmus1 <semihh334@gmail.com>
2023-07-26 19:40:25 +0300

    Date Login Solved
    Tarihe göre login değeri tutma olayı tanımlandı. Artık günde en fazla 3 defa girebiliyor, ve sonra giremiyor. Lakin yarın olduğunda login değeri sıfırlanıyor ve yeniden girebiliyor.

--- Space Defenders/Assets/Scripts/LoginManager.cs
+++ Space Defenders/Assets/Scripts/LoginManager.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class LoginManager : MonoBehaviour
 {
     private int loginCount;
+    private int loginDate;
+    private int currentDate;
 
     private void Start()
     {
-        LoginOku();
+        currentDate = Convert.ToInt16(DateTime.Today.Day);
 
+        LoginOku();
         loginCount = Convert.ToInt16(LoginOku());
 
+        //DateOku();
+        loginDate = Convert.ToInt16(DateOku());
+        Debug.Log("Current Date: " + currentDate);
+        Debug.Log("Last Login Date: " + loginDate);
+
+
         LoginSorgula();
+
+
+        if (loginDate != currentDate)
+        {
+            Debug.Log("Login Count Sýfýrlandý.");
+            LoginYaz(1);
+            DateYaz();
+        }
     }
 
     private void LoginSorgula()
     {
         print("LogCount: " + loginCount);
-        if (loginCount > 3)
+        if (loginCount >= 3 && loginDate == currentDate)
         {
             Debug.Log("Oyuna giremezsin");
             Application.Quit();
@@ -45,7 +62,6 @@ public class LoginManager : MonoBehaviour
         else
         {
             LoginYaz(1);
-            Debug.Log("Veri yoktu, bu yüzden 0 yazýldý");
             return "0";
         }
     }
@@ -57,5 +73,38 @@ public class LoginManager : MonoBehaviour
             FileStream fs = File.Create("loginCount.txt");
             fs.Close();
         }
+    }
+
+
+    private static void DosyaYoksaOlusturDate()
+    {
+        if (!File.Exists("loginDate.txt"))
+        {
+            FileStream fs = File.Create("loginDate.txt");
+            fs.Close();
+        }
+    }
+
+    private string DateOku()
+    {
+        DosyaYoksaOlusturDate();
+
+        StreamReader srDate = new StreamReader("loginDate.txt");
+        string veriDate = srDate.ReadLine();
+        srDate.Close();
+        if (veriDate != null) { return veriDate; }
+        else
+        {
+            DateYaz();
+            Debug.Log("Dosya boþ olduðundan bugünün Datei Yazýldý: " + DateTime.Today.Day);
+            return "0";
+        }
+    }
+
+    private static void DateYaz()
+    {
+        StreamWriter swDate = new StreamWriter("loginDate.txt");
+        swDate.Write(DateTime.Today.Day);
+        swDate.Close();
     }
 }
